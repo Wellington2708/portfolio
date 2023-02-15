69370f7dc7723528d46028e61d9a75ed8c908d96
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-02-15 21:56:29 +0000

    colocando carrossel em skills.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,16 @@ botao.addEventListener("click" , sobreMim);
 function sobreMim(){
     var sobreMim = document.querySelector(".quem__sou");
     sobreMim.innerHTML= `
+    <style>
+
+        .fechar__botao {
+            color: rgb(203 233 224)
+        }
+    
+    </style>
     <div class="fechar__botao">
         <p>Olá, meu nome é Wellington Junior,sou natural de Goiânia - Goiás e formado em Gestão de Pessoas. 
-        Tenho <span>34</span> anos e decidi migrar para a área de tecnologia. <br>
+        Tenho 34 anos e decidi migrar para a área de tecnologia. <br>
         Venho preparando-me para ser um Desenvolvedor Front-End aprendendo diariamente com teoria e prática.
         Prática essa que me leva a desenvolver projetos utilizando, por enquanto, HTML, CSS e Javascript.<br>
         <button onclick="fechar()" class="fechar">Fechar</button>
